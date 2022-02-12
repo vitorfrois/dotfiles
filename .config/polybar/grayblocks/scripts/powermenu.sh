824c4e09e1f809c3268af13368f87582ad224c3c
vitorfrois <vitor0frois@gmail.com>
2022-02-12 17:13:18 -0300

    gruvbox theme att

---
+++ .config/polybar/grayblocks/scripts/powermenu.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+## Author  : Aditya Shakya
+## Mail    : adi1090x@gmail.com
+## Github  : @adi1090x
+## Twitter : @adi1090x
+
+exec ~/.config/rofi/powermenu/powermenu.sh
---
+++ .config/polybar/grayblocks/scripts/xxxwindow.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+WM_DESKTOP=$(xdotool getwindowfocus)
+
+if [ $WM_DESKTOP == "4194367" ]; then
+
+	echo "Desktop"
+
+elif [ $WM_DESKTOP != "4194367" ]; then
+
+	WM_CLASS=$(xprop -id $(xdotool getactivewindow) WM_CLASS | awk 'NF {print $NF}' | sed 's/"/ /g')
+	WM_NAME=$(xprop -id $(xdotool getactivewindow) WM_NAME | cut -d '=' -f 2 | awk -F\" '{ print $2 }')
+
+	if [ $WM_CLASS == 'code-oss' ]; then
+		echo "%{F}Visual Studio Code%{u-}"
+
+	elif [ $WM_CLASS == 'Alacritty' ]; then
+		echo "%{F}Alacritty%{u-}"
+
+	elif [ $WM_CLASS == 'firefox' ]; then
+		echo "%{F}Firefox%{u-}"
+
+	elif [ $WM_CLASS == 'Thunderbird' ]; then
+		echo "%{F}Thunderbird%{u-}"
+	
+	# elif [ $WM_NAME == 'i3' ]; then
+
+	# 	echo "%{F#ffffff}Customname%{u-}"
+
+	else
+		echo "%{F}$WM_NAME%{u-}"
+
+	fi
+
+fi
